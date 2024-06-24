f575cdbb3ae1771565e85ebb4bfafee7298a493e
martinlaxenaire <martin.laxenaire@gmail.com>
2024-06-24 15:37:22 +0200

    11. Gallery - adding post processing

--- js/planes-scene/PlanesScene.js
+++ js/planes-scene/PlanesScene.js
@@ -1,7 +1,8 @@
-import { Plane } from 'gpu-curtains'
+import { Plane, Sampler, ShaderPass } from 'gpu-curtains'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import { DemoScene } from '../DemoScene'
 import { planesFs, planesVs } from '../shaders/gallery-planes.wgsl'
+import { galleryShaderPassFs } from '../shaders/gallery-shader-pass.wgsl'
 
 export class PlanesScene extends DemoScene {
   constructor({ renderer }) {
@@ -36,12 +37,31 @@ export class PlanesScene extends DemoScene {
 
       this.planes.push(plane)
     })
+
+    this.shaderPass = new ShaderPass(this.renderer, {
+      label: 'Distortion shader pass',
+      shaders: {
+        fragment: {
+          code: galleryShaderPassFs,
+        },
+      },
+      samplers: [
+        new Sampler(this.renderer, {
+          label: 'Clamp sampler',
+          name: 'clampSampler',
+          addressModeU: 'clamp-to-edge',
+          addressModeV: 'clamp-to-edge',
+        }),
+      ],
+    })
   }
 
   destroyWebGPU() {
     this.planes.forEach((plane) => {
       plane.remove()
     })
+
+    this.shaderPass.remove()
   }
 
   addScrollTrigger() {
